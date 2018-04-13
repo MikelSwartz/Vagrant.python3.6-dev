ef0b582d7c8cc6ec9d4de558849ecfde9e724d69
Mike <mikelswartz@gmail.com>
2018-04-12 22:53:53 -0400

    Added IDE-like vim configuration for python

--- py.provision.sh
+++ py.provision.sh
@@ -10,3 +10,9 @@ yum install python36u-devel -y
 echo "Creating python3 alias for python3's REPL"
 echo "'py'"
 echo "alias py='python3'" >> .bashrc
+
+cd /home/vagrant/
+sudo -u vagrant git clone https://github.com/spf13/spf13-vim.git
+cd /home/vagrant/spf13-vim
+sudo -u vagrant chmod +x bootstrap.sh
+sudo -u vagrant ./bootstrap.sh
